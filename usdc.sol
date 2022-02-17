9893fe33e0e43a67054051fd3b149cd1ac6fffad
Thinhbp <90796376+Thinhbp@users.noreply.github.com>
2022-02-17 17:12:05 +0700

    Update usdc.sol

--- usdc.sol
+++ usdc.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MintableToken is  ERC20 {
+contract usdc is  ERC20 {
     constructor() public ERC20("usdc", "USDC") {
     }
 
