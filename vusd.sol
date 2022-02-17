379a8baa15fb33a02c8337678c389224243e46f3
Thinhbp <90796376+Thinhbp@users.noreply.github.com>
2022-02-17 15:49:06 +0700

    Update vusd.sol

--- vusd.sol
+++ vusd.sol
@@ -17,8 +17,12 @@ contract vusd is  ERC20 {
 
     address public addressVNC;
 
+
+ 
+
+
     address  USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-    // 0x38558FB189f9fB0a6B455064477627Fdbe3d0f1cFor Testnet
+    // 0x38558FB189f9fB0a6B455064477627Fdbe3d0f1c for Testnet
 
     event buy(address _address, uint _amount);
     event sell(address _address, uint _amount);
@@ -30,8 +34,8 @@ contract vusd is  ERC20 {
     function buyToken(uint _amount) public {
         require(status, "Contract is maintaining");
         require(_amount > 0);
-        require(IERC20(USDC).allowance(msg.sender, address(this)) == _amount*10**18,"You must approve in web3");
-        require(IERC20(USDC).transferFrom(msg.sender, address(this),_amount*10**18), "Transfer failed");
+        //require(IERC20(USDC).allowance(msg.sender, address(this)) == _amount*10**18,"You must approve in web3");
+        //require(IERC20(USDC).transferFrom(msg.sender, address(this),_amount*10**18), "Transfer failed");
         _mint(msg.sender, _amount*10**18);
         emit buy(msg.sender, _amount);
 
@@ -49,7 +53,7 @@ contract vusd is  ERC20 {
 
     function withdraw() public {
         require(msg.sender == addresswithdraw,"permission denied");
-        uint moneyCanWithdraw = vnc(addressVNC).moneyIcanUse();
+        uint moneyCanWithdraw = vnc(addressVNC).moneyCanWithdraw();
         require(moneyCanWithdraw > moneyWithdrawed);
         IERC20(USDC).transfer(msg.sender, (moneyCanWithdraw - moneyWithdrawed));
         moneyWithdrawed += (moneyCanWithdraw - moneyWithdrawed);
@@ -88,6 +92,6 @@ contract vusd is  ERC20 {
 } 
 
 abstract contract vnc {
-    function moneyIcanUse() public virtual  returns(uint);
+    function moneyCanWithdraw() public virtual  returns(uint);
 }
 
